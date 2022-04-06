b3ecab21a3924fbd6d2f4e19b0c655e94786542b
Quentin Pahaut <quentinpahaut@gmail.com>
2022-04-06 15:49:21 +0200

    ADD: Communication and parsing serial data

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 "use strict"
 
-
+//import node libs
 const { SerialPort } = require('serialport'); 
-console.log(SerialPort);
+const { ReadlineParser} = require ('@serialport/parser-readline');
+
+/*
+--== SerialPort Instance ==--
+Used to set Serial port communication
+
+*/ 
 
 const port = new SerialPort({
     path: '/dev/ttyACM0',
     baudRate: 9600,
-})
+    function (err) {
+        if (err) {
+            return console.log('Error :', err.message);
+        }
+    }
+});
+
+/*
+--== ReadlineParser Instance ==--
+Used to emit data after newline (arduino:println) delimiter received.
+Default encoding option is : UTF8.
+
+*/ 
+const parser = new ReadlineParser({delimiter:"\n"});
+
+port.pipe(parser);
+
+//Set comminication usig parse (every newline received)
+parser.on('data', function (data){
+    
+    //hexa to float conversion
+    let bikeOutput = parseFloat(data);
+    
+    if (bikeOutput > 0){
+        console.log(bikeOutput);
+    }
+});
+
+
